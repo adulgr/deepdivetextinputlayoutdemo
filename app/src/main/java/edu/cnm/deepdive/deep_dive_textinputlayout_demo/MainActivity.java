814946dbd7a9e0330993b999a2421e39078f37ff
adulgr <Dloko505@gmail.com>
2018-08-03 08:47:21 -0600

    Initial commit of edit text example app

---
+++ app/src/main/java/edu/cnm/deepdive/deep_dive_textinputlayout_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.deep_dive_textinputlayout_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
